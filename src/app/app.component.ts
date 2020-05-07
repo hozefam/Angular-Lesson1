0e5c28aca45e3bbf3d22f1d4c2f46c2f3d9a1346
Hozefa <hozefam@gmail.com>
2020-05-07 20:37:33 +0300

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,40 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'live';
+  number1: number;
+  number2: number;
+  result = 0;
+  operator = '+'; // default operator
+
+  onOperationClick(operation: string) {
+    this.operator = operation;
+    this.calculate();
+  }
+
+  onClear() {
+    this.number1 = null;
+    this.number2 = null;
+  }
+
+  calculate() {
+    switch (this.operator) {
+      case '+':
+        this.result = this.number1 + this.number2;
+        break;
+      case '-':
+        this.result = this.number1 - this.number2;
+        break;
+      case '*':
+        this.result = this.number1 * this.number2;
+        break;
+      case '/':
+        this.result = this.number1 / this.number2;
+        break;
+      default:
+        break;
+    }
+  }
 }
